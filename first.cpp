12b3636880a25c9d47dc9f9ed6bfc31f97ba4c84
Shreya Singh <iamshreyaa06gmail.com>
2023-08-07 23:33:20 -0700

    initial commit

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() { 
+    int a, b, c;
+    cout<<"Enter first number: "<<endl;
+    cin>>a;
+    cout<<"Enter second number: "<<endl;
+    cin>>b;
+    c = a + b;
+    cout<<"The sum is: "<<c<<endl;
+    return 0;
+}
